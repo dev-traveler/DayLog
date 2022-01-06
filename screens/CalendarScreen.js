373ad1c12d6354e672285ac9379813dc1ff751d3
dev-traveler <avenj.dev@gmail.com>
2022-01-06 22:27:47 +0900

    Hide FloatingWriteButton when scroll is about in FlatList bottom

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -1,20 +1,44 @@
 import React, {useEffect, useRef, useState} from 'react';
 import {Animated, Button, StyleSheet, View} from 'react-native';
 
-function FadeInAndOut() {
-  const animation = useRef(new Animated.Value(1)).current;
-  const [hidden, setHidden] = useState(false);
+function SlideLeftAndRight() {
+  const animation = useRef(new Animated.Value(0)).current;
+  const [enabled, setEnabled] = useState(false);
+
   useEffect(() => {
     Animated.timing(animation, {
-      toValue: hidden ? 0 : 1,
+      toValue: enabled ? 1 : 0,
       useNativeDriver: true,
     }).start();
-  }, [hidden, animation]);
+  }, [enabled, animation]);
 
   return (
     <View>
-      <Animated.View style={[styles.rectangle, {opacity: animation}]} />
-      <Button title="Toggle" onPress={() => setHidden(!hidden)} />
+      <Animated.View
+        style={[
+          styles.rectangle,
+          {
+            transform: [
+              {
+                translateX: animation.interpolate({
+                  inputRange: [0, 1],
+                  outputRange: [0, 150],
+                }),
+              },
+            ],
+            opacity: animation.interpolate({
+              inputRange: [0, 1],
+              outputRange: [1, 0],
+            }),
+          },
+        ]}
+      />
+      <Button
+        title="Toggle"
+        onPress={() => {
+          setEnabled(!enabled);
+        }}
+      />
     </View>
   );
 }
@@ -22,7 +46,7 @@ function FadeInAndOut() {
 function CalendarScreen() {
   return (
     <View style={styles.block}>
-      <FadeInAndOut />
+      <SlideLeftAndRight />
     </View>
   );
 }
