855e69adff37085bdce00c93cb060260cbcbb398
dev-traveler <avenj.dev@gmail.com>
2022-01-09 18:04:18 +0900

    Add component for empty search result

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -3,6 +3,7 @@ import {StyleSheet, View} from 'react-native';
 import FeedList from '../components/FeedList';
 import LogContext from '../contexts/LogContext';
 import SearchContext from '../contexts/SearchContext';
+import EmptySearchResult from '../components/EmptySearchResult';
 
 function SearchScreen() {
   const {keyword} = useContext(SearchContext);
@@ -14,6 +15,14 @@ function SearchScreen() {
       : logs.filter(log =>
           [log.title, log.body].some(text => text.includes(keyword)),
         );
+
+  if (keyword === '') {
+    return <EmptySearchResult type="EMPTY_KEYWORD" />;
+  }
+
+  if (filtered.length === 0) {
+    return <EmptySearchResult type="NOT_FOUND" />;
+  }
 
   return (
     <View style={styles.block}>
